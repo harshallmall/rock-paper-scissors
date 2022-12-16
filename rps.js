40b7e9458e75546dd7a81f242a34d41f59aa1d74
harshallmall <hall1705@umn.edu>
2022-12-16 16:18:53 -0600

    update on rps-ui

--- rps.js
+++ rps.js
@@ -1,76 +1,79 @@
-// create the variable that holds the array of choices
-const options = ["#rock", "#paper", "#scissors"];
-
-// game score options
+// Create two "let" variables to hold each player's score, as the variables will ultimately increment in value.
+// Create a "constant" variable that holds all three choices.
+// Add an event listener to each one of the three choices.
 let playerScore = 0;
 let computerScore = 0;
-let ties = 0;
-
-// function to define computer's selection parameters
-function computerPlay() {
-    const computerOptions = options[Math.floor(Math.random() * options.length)];
-    return computerOptions;
-}
-
-// function to define player's selection parameters
-function playerPlay() {
-    const playerOptions = prompt("Rock, Paper, Scissors");
-    const playerChoices = playerOptions.toLowerCase();
-    return playerOptions || playerChoices;
-}
-
-// function to define the parameters of one round of game play
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        ties++;
+const choices = document.querySelectorAll('.choices');
+// The callback function passed into addEventListener() method executes when a click occurs on one of the three choices.
+// After the click event occurs, the selection is fetched and read via the value property.
+// The value assigned to the choice is stored as a variable called playerChoice.
+// The computer is then assigned three choices in an array, then the numbers of the choices are randomized.
+// Call separate function that will be created below to invoke the comparison of choices and decision of winner/loser.
+// Call separate function that will update the score of the players.
+// Call seperate function that will check if a player has won based on the updated score.
+choices.forEach((choice) => {
+    choice.addEventListener("click", function () {
+        const playerChoice = this.value;
+        const computerChoices = ["Rock", "Paper", "Scissors"];
+        const computerChoice = computerChoices[Math.floor(Math.random() * 3)];
+        gameChoices(playerChoice, computerChoice);
+        gameScore();
+        if (gameWinner()) {
+            alert(`${winner}`);
+            playerScore = computerScore = 0;
+            gameScore();
+        }
+    });
+});
+// Create a function to compare the choices between the two players and decide the winner of (x) round.
+// The possibility of a tie is separate from the other choices and shortens the necessary if/else statement for comparison.
+// Increment the score for either player depending on the outcome of the comparisons.
+function gameChoices(playerChoice, computerChoice) {
+    const gameChoice = `${playerChoice} vs. ${computerChoice}`;
+    if (playerChoice === computerChoice) {
+        alert(`${gameChoice} = Tie`);
+        return;
     }
-
-    if (playerSelection == options[0] && computerSelection == options[2]) {
-        playerScore++;
-        return "Player wins round";
-    } else if (playerSelection == options[0] && computerSelection == options[1]) {
-        computerScore++;
-        return "Computer wins round";
-    } else if (playerSelection == options[1] && computerSelection == options[0]) {
-        playerScore++;
-        return "Player wins round";
-    } else if (playerSelection == options[1] && computerSelection == options[2]) {
-        computerScore++;
-        return "Computer wins round";
-    } else if (playerSelection == options[2] && computerSelection == options[0]) {
-        computerScore++;
-        return "Computer wins round";
-    } else if (playerSelection == options[2] && computerSelection == options[1]) {
-        playerScore++;
-        return "Player wins round";
-    } else {
-        return "Play Again"
+    if (playerChoice === "Rock") {
+        if (computerChoice === "Scissors") {
+            alert(`${gameChoice} = Player wins`);
+            playerScore++;
+        } else {
+            alert(`${gameChoice} = Player loses`);
+            computerScore++
+        }
+    }
+    else if (playerChoice === "Paper") {
+        if (computerChoice === "Scissors") {
+            alert(`${gameChoice} = Player wins`);
+            playerScore++;
+        } else {
+            alert(`${gameChoice} = Player loses`);
+            computerScore++;
+        }
+    }
+    else if (playerChoice === "Scissors") {
+        if (computerChoice === "Rock") {
+            alert(`${gameChoice} = Player wins`);
+            playerScore++;
+        } else {
+            alert(`${gameChoice} = Player loses`);
+            computerScore++;
+        }
     }
 }
-
-// function to define the game for five rounds with score keeping
-function game() {
-    for (let i = 0; i <= 5; i++) {
-        let playerSelection = playerPlay();
-        let computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-        console.log(`[Round ${i}] Player: ${playerSelection} vs. Computer: ${computerSelection}`);
-        console.log(`[Score] Player: ${playerScore} vs. Computer: ${computerScore}`);
-    }
-    if (playerScore === 3 && computerScore <= 2) {
-        console.log("Player wins game");
-    } 
-    if (computerScore === 3 && playerScore <= 2) {
-        console.log("Computer wins game")
-    }
+// Create a function to update each player's score after each round using the elements created in HTML.
+function gameScore() {
+    document.getElementById("playerScore").textContent = playerScore;
+    document.getElementById("computerScore").textContent = computerScore;
 }
-
-// initialize the game function inside the console
-game();
-
-const clickOne = document.querySelector("#rock"); 
-clickOne.addEventListener('click', () => {playRound;});
-const clickTwo = document.querySelector("#paper");
-clickTwo.addEventListener('click', () => {playRound;});
-const clickThree = document.querySelector("scissors");
-clickThree.addEventListener("click", () => {playRound;}); 
+// Create a function to check if a player reaches five points.
+// Use the ternary conditional operator (?) to choose an alternative winning message depending on score.
+// Update the score in the round play function.
+function gameWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+        const winner = playerScore === 5 ? "Player wins" : "Computer wins";
+        return true;
+    }
+    return false;
+}
